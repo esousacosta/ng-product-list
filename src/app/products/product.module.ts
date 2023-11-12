7f82bef43c237b58ee74ae313714e25f2e2af11a
esousacosta <victoremanoel23@gmail.com>
2023-11-12 21:51:51 +0100

    Create shared module and import it into product mod

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 // Local
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
-import { StarComponent } from '../shared/start.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
 import { productDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
     ConvertToSpacesPipe,
-    StarComponent,
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       {
@@ -27,6 +22,7 @@ import { productDetailGuard } from './product-detail.guard';
         component: ProductDetailComponent,
       },
     ]),
+    SharedModule,
   ],
 })
 export class ProductModule {}
